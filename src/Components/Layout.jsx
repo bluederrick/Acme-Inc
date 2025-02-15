fb89c2b17a944a5516be9d49851ed833e44585bf
derrick nsirim <derricknsirim77@gmail.com>
2025-02-15 22:49:21 +0100

    product component added with pagination

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,27 +1,5 @@
 /** @format */
-<<<<<<< HEAD
 
-import React from 'react'
-import AppHeader from './AppHeader'
-import SideMenu from './SideMenu'
-import PageContent from './PageContent'
-
-const Layout = () => {
-  return (
-    <div className="flex h-screen">
-      {/* Sidebar */}
-      <SideMenu />
-
-      {/* Main content section */}
-      <div className="flex-1 flex flex-col">
-        {/* Header */}
-        <AppHeader />
-
-        {/* Page content */}
-        <div className="flex-1 flex flex-col h-2/5 ">
-          <PageContent />
-=======
-import React from 'react'
 import AppHeader from './AppHeader'
 import SideMenu from './SideMenu'
 import { Outlet, useLocation } from 'react-router-dom'
@@ -41,7 +19,6 @@ const Layout = () => {
         {/* Outlet will render the nested route component */}
         <div className="flex-1 flex flex-col h-2/5 ">
           <Outlet />
->>>>>>> lost-commit
         </div>
       </div>
     </div>
